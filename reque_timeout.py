55da93f18e1ebbee9f54277dab8d0dbb198cc911
huj <759781714@qq.com>
2017-03-24 16:54:02 +0800

    Update reque_timeout.py

--- reque_timeout.py
+++ reque_timeout.py
@@ -1,5 +1,5 @@
 #coding:utf-8
 #使用timeout参数设置超时时间
 import requests
-response = requests.get('http://www.github.com',timeout=1)
+response = requests.get('http://www.github.com',timeout=1)#time较小就会出错
 print response.status_code
