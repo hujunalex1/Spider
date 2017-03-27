40e148caf51a645d021bfc43d344660ffcc04851
hujun <759781714@qq.com>
2017-03-27 09:56:01 +0800

    增加一些headers用法

--- request_headers.py
+++ request_headers.py
@@ -3,11 +3,12 @@
 import requests
 import json
 
-url = 'https://api.github.com/some/endpoint'
+url = 'http://httpbin.org/post'
 data = {'some':'data'}
 headers = {'content-type':'application/json'}
 response = requests.post(url,data=json.dumps(data),headers=headers)
 print response.status_code
 print response.text
 print response.headers
+print response.headers.get('content-type')
 
