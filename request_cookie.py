7aea2366976eaaa0d4e85306844cbf011e243350
hujun <759781714@qq.com>
2017-03-27 10:13:45 +0800

    添加重定向选择

--- request_cookie.py
+++ request_cookie.py
@@ -2,5 +2,7 @@
 #获取响应中得cookies
 import requests
 url = 'http://www.baidu.com'
-response = requests.get(url)
-print  response.cookies
+response = requests.get(url,allow_redirects=False)#允许重定向
+print response.history
+print response.status_code
+print response.cookies
